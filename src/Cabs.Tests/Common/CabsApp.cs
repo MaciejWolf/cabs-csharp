9fcdba51098c4d6c031811562be82efcce6ae73b
MaciejWolf <m_wolf@interia.pl>
2022-02-11 16:20:47 +0100

    2.1.3 initial setup for integration tests

---
+++ src/Cabs.Tests/Common/CabsApp.cs
@@ -0,0 +1,30 @@
+﻿using LegacyFighter.Cabs.Service;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cabs.Tests.Common;
+
+internal class CabsApp : WebApplicationFactory<Program>
+{
+    private IServiceScope _scope;
+
+    private CabsApp()
+    {
+        _scope = base.Services.CreateAsyncScope();
+    }
+    public static CabsApp CreateInstance() => new();
+
+    protected override void Dispose(bool disposing)
+    {
+        _scope.Dispose();
+        base.Dispose(disposing);
+    }
+    private IServiceScope NewRequestScope()
+    {
+        _scope.Dispose();
+        _scope = Services.CreateAsyncScope();
+        return _scope;
+    }
+    public IDriverService DriverService
+      => NewRequestScope().ServiceProvider.GetRequiredService<IDriverService>();
+}
