8ad7eea899d493304f1be1ce826160a2b898a30d
MaciejWolf <m_wolf@interia.pl>
2022-02-11 18:09:06 +0100

    2.1 Value object DriverLicense

---
+++ src/Cabs.Tests/Entity/DriverLicenseTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using LegacyFighter.Cabs.Entity;
+using System;
+using Xunit;
+
+namespace Cabs.Tests.Entity;
+
+public class DriverLicenseTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("InvalidLicense")]
+    public void CannotCreateInvalidDriverLicense(string license)
+    {
+        var act = () => DriverLicense.WithLicense(license);
+        act.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void CanCreateValidDriverLicense()
+    {
+        const string ValidLicense = "FARME100165AB5EW";
+
+        var driverLicense = DriverLicense.WithLicense(ValidLicense);
+
+        driverLicense.AsString().Should().Be(ValidLicense);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("InvalidLicense")]
+    public void CanCreateInvalidDriverLicenseExplicitly(string license)
+    {
+        var driverLicense = DriverLicense.WithoutValidation(license);
+
+        driverLicense.AsString().Should().Be(license);
+    }
+}
---
+++ src/Cabs/Entity/DriverLicense.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LegacyFighter.Cabs.Entity;
+
+public record DriverLicense
+{
+    private const string DriverLicenseRegex = "^[A-Z9]{5}\\d{6}[A-Z9]{2}\\d[A-Z]{2}$";
+
+    private readonly string value;
+
+    private DriverLicense(string value)
+    {
+        this.value = value;
+    }
+
+    public static DriverLicense WithLicense(string driverLicense)
+    {
+        if (!IsValid(driverLicense))
+            throw new ArgumentException("Illegal license no = " + driverLicense);
+
+        return new DriverLicense(driverLicense);
+    }
+
+    public static DriverLicense WithoutValidation(string driverLicense) => new(driverLicense);
+
+    private static bool IsValid(string license) 
+        => license is not null && license.Any() && Regex.IsMatch(license, DriverLicenseRegex);
+
+    public string AsString() => value;
+}
