9c7ba6f3db284d3b4b8400d813bd206bf2a3d321
MaciejWolf <m_wolf@interia.pl>
2022-02-11 15:59:14 +0100

    1 - replace nUnit with xUnit

---
+++ src/Cabs.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Cabs.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- src/CabsTests/Globals.cs
+++
@@ -1,3 +0,0 @@
-﻿global using NUnit.Framework;
-global using FluentAssertions;
-global using NSubstitute;
