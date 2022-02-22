a133b29f300472c72738b17c1c40e7719db5afef
MaciejWolf <m_wolf@interia.pl>
2022-02-22 17:38:34 +0100

    add test

--- src/Cabs.Tests/Ui/CalculateTransitDistanceTests.cs
+++ src/Cabs.Tests/Ui/CalculateTransitDistanceTests.cs
@@ -17,29 +17,38 @@ public class CalculateTransitDistanceTests
     [Fact]
     public void ShouldThrowExceptionWhenUnitIsInvalid()
     {
-        var act = () => TransitForDistance(50).GetDistance("invalid");
+        var act = () => TransitForDistanceOfKm(50).GetDistance("invalid");
         act.Should().ThrowExactly<ArgumentException>();
     }
 
     [Fact]
     public void ShouldPresentAsKm()
     {
-        TransitForDistance(50).GetDistance("km").Should().Be("50km");
-        TransitForDistance(150).GetDistance("km").Should().Be("150km");
-        TransitForDistance(55.7f).GetDistance("km").Should().Be("55.700km");
-        TransitForDistance(0).GetDistance("km").Should().Be("0km");
+        TransitForDistanceOfKm(50).GetDistance("km").Should().Be("50km");
+        TransitForDistanceOfKm(150).GetDistance("km").Should().Be("150km");
+        TransitForDistanceOfKm(55.7f).GetDistance("km").Should().Be("55.700km");
+        TransitForDistanceOfKm(0).GetDistance("km").Should().Be("0km");
+    }
+
+    [Fact]
+    public void ShouldPresentAsMeters()
+    {
+        TransitForDistanceOfKm(50).GetDistance("m").Should().Be("50000m");
+        TransitForDistanceOfKm(150).GetDistance("m").Should().Be("150000m");
+        TransitForDistanceOfKm(55.7f).GetDistance("m").Should().Be("55700m");
+        TransitForDistanceOfKm(0).GetDistance("m").Should().Be("0m");
     }
 
     [Fact]
     public void ShouldPresentAsMiles()
     {
-        TransitForDistance(10).GetDistance("miles").Should().Be("6.214miles");
-        TransitForDistance(10.123f).GetDistance("miles").Should().Be("6.290miles");
-        TransitForDistance(10.12345f).GetDistance("miles").Should().Be("6.290miles");
-        TransitForDistance(0).GetDistance("miles").Should().Be("0miles");
+        TransitForDistanceOfKm(10).GetDistance("miles").Should().Be("6.214miles");
+        TransitForDistanceOfKm(10.123f).GetDistance("miles").Should().Be("6.290miles");
+        TransitForDistanceOfKm(10.12345f).GetDistance("miles").Should().Be("6.290miles");
+        TransitForDistanceOfKm(0).GetDistance("miles").Should().Be("0miles");
     }
 
-    private static TransitDto TransitForDistance(float km)
+    private static TransitDto TransitForDistanceOfKm(float km)
     {
         var transit = new Transit
         {
