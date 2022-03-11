7dd24a1241f810b1770b544ab78a027c1793e000
MaciejWolf <maciej.wolf@outlook.com>
2022-03-11 21:44:16 +0100

    refactor Tariff

--- src/Cabs/Values/Tariff.cs
+++ src/Cabs/Values/Tariff.cs
@@ -21,33 +21,14 @@ public record Tariff
         _factor = factor;
     }
 
-    public static Tariff Create(LocalDateTime date, int? factor = null)
+    public static Tariff Create(LocalDateTime day, int? factor = null)
     {
-        var (name, kmRate) = GetTariff(date);
-        var baseFee = CalculateBaseFee(date);
+        factor ??= 1;
 
-        return new Tariff(name, kmRate, baseFee, factor ?? 1);
-    }
-
-    public Money CalculateCost(Distance distance)
-    { 
-        var pricedecimal = new decimal(distance.Km * KmRate * _factor + _baseFee);
-        pricedecimal = decimal.Round(pricedecimal, 2, MidpointRounding.ToPositiveInfinity);
-        var finalPrice = int.Parse(pricedecimal.ToString("0.00", CultureInfo.InvariantCulture).Replace(".", ""));
-        return Money.OfValue(finalPrice);
-    }
-
-    private static (string Name, float KmRate) Standard => ("Standard", 1.0f);
-    private static (string Name, float KmRate) NewYearsEve => ("Sylwester", 3.50f);
-    private static (string Name, float KmRate) Weekend => ("Weekend", 1.5f);
-    private static (string Name, float KmRate) WeekendPlus => ("Weekend+", 2.50f);
-
-    private static (string Name, float KmRate) GetTariff(LocalDateTime day)
-    {
         // wprowadzenie nowych cennikow od 1.01.2019
         if (day.Year <= 2018)
         {
-            return Standard;
+            return StandardTariff(factor.Value);
         }
 
         var year = day.Year;
@@ -56,7 +37,7 @@ public record Tariff
         if (((leap && day.DayOfYear == 366) || (!leap && day.DayOfYear == 365)) ||
             (day.DayOfYear == 1 && day.Hour <= 6))
         {
-            return NewYearsEve;
+            return NewYearsEveTariff(factor.Value);
         }
         else
         {
@@ -66,83 +47,53 @@ public record Tariff
                 case IsoDayOfWeek.Tuesday:
                 case IsoDayOfWeek.Wednesday:
                 case IsoDayOfWeek.Thursday:
-                    return Standard;
+                    return StandardTariff(factor.Value);
                 case IsoDayOfWeek.Friday:
                     if (day.Hour < 17)
                     {
-                        return Standard;
+                        return StandardTariff(factor.Value);
                     }
                     else
                     {
-                        return WeekendPlus;
+                        return WeekendPlusTariff(factor.Value);
                     }
                 case IsoDayOfWeek.Saturday:
                     if (day.Hour < 6 || day.Hour >= 17)
                     {
-                        return WeekendPlus;
+                        return WeekendPlusTariff(factor.Value);
                     }
-                    else/* if (day.Hour < 17)*/
+                    else
                     {
-                        return Weekend;
+                        return WeekendTariff(factor.Value);
                     }
                 case IsoDayOfWeek.Sunday:
                     if (day.Hour < 6)
                     {
-                        return WeekendPlus;
+                        return WeekendPlusTariff(factor.Value);
                     }
                     else
                     {
-                        return Weekend;
+                        return WeekendTariff(factor.Value);
                     }
 
                 default:
-                    return (default, default);
+                    throw new ArgumentException();
             }
         }
     }
 
-    private static int CalculateBaseFee(LocalDateTime day)
+    private static Tariff StandardTariff(int factor) => new("Standard", 1.0f, 9, factor);
+    private static Tariff NewYearsEveTariff(int factor) => new("Sylwester", 3.50f, 11, factor);
+    private static Tariff WeekendTariff(int factor) => new("Weekend", 1.5f, 8, factor);
+    private static Tariff WeekendPlusTariff(int factor) => new("Weekend+", 2.50f, 10, factor);
+
+
+    public Money CalculateCost(Distance distance)
     {
-        var baseFee = BaseFee;
-
-        if (day.Year <= 2018)
-        {
-            baseFee++;
-        }
-        else
-        {
-            if ((day.Month == 12 && day.Day == 31) ||
-                (day.Month == 1 && day.Day == 1 && day.Hour <= 6))
-            {
-                baseFee += 3;
-            }
-            else
-            {
-                // piątek i sobota po 17 do 6 następnego dnia
-                if ((day.DayOfWeek == IsoDayOfWeek.Friday && day.Hour >= 17) ||
-                    (day.DayOfWeek == IsoDayOfWeek.Saturday && day.Hour <= 6) ||
-                    (day.DayOfWeek == IsoDayOfWeek.Saturday && day.Hour >= 17) ||
-                    (day.DayOfWeek == IsoDayOfWeek.Sunday && day.Hour <= 6))
-                {
-                    baseFee += 2;
-                }
-                else
-                {
-                    // pozostałe godziny weekendu
-                    if ((day.DayOfWeek == IsoDayOfWeek.Saturday && day.Hour > 6 && day.Hour < 17) ||
-                        (day.DayOfWeek == IsoDayOfWeek.Sunday && day.Hour > 6))
-                    {
-
-                    }
-                    else
-                    {
-                        // tydzień roboczy
-                        baseFee++;
-                    }
-                }
-            }
-        }
-
-        return baseFee;
+        var pricedecimal = new decimal(distance.Km * KmRate * _factor + _baseFee);
+        pricedecimal = decimal.Round(pricedecimal, 2, MidpointRounding.ToPositiveInfinity);
+        var finalPrice = int.Parse(pricedecimal.ToString("0.00", CultureInfo.InvariantCulture).Replace(".", ""));
+        return Money.OfValue(finalPrice);
     }
+
 }
