20b9d5f3d85cc2d2dbf1f12210333352643f7540
MaciejWolf <maciej.wolf@outlook.com>
2022-03-11 16:01:04 +0100

    TariffTests for recognizing tariff

---
+++ src/Cabs.Tests/Values/TariffTests.cs
@@ -0,0 +1,34 @@
+﻿using Cabs.Tests.Common.TestData;
+using FluentAssertions;
+using LegacyFighter.Cabs.Values;
+using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Cabs.Tests.Values;
+
+public class TariffTests
+{
+    [Theory]
+    [MemberData(nameof(DatesWithTariffs))]
+    public void ShouldHaveValidTariffName(LocalDateTime date, string expectedName, float expectedKmRate)
+    {
+        var tariff = Tariff.Create(date);
+        tariff.Name.Should().Be(expectedName);
+        tariff.KmRate.Should().Be(expectedKmRate);
+    }
+
+    public static IEnumerable<object[]> DatesWithTariffs()
+    {
+        var standard = Dates.WithStandardTariff.Select(HasTariffAndKmRate("Standard", 1.0f));
+        var weekend = Dates.WithWeekendTariff.Select(HasTariffAndKmRate("Weekend", 1.5f));
+        var weekendPlus = Dates.WithWeekendPlusTariff.Select(HasTariffAndKmRate("Weekend+", 2.50f));
+        var newYearsEve = Dates.WithNewYearsEveTariff.Select(HasTariffAndKmRate("Sylwester", 3.50f));
+
+        return standard.Concat(weekend).Concat(weekendPlus).Concat(newYearsEve);
+    }
+
+    public static Func<LocalDateTime, object[]> HasTariffAndKmRate(string tariff, float kmRate) => date => new object[] { date, tariff, kmRate };
+}
---
+++ src/Cabs/Values/Tariff.cs
@@ -0,0 +1,87 @@
+﻿using NodaTime;
+
+namespace LegacyFighter.Cabs.Values;
+
+public record Tariff
+{
+    public string Name { get; }
+    public float KmRate { get; }
+
+    private Tariff(string name, float kmRate)
+    {
+        Name = name;
+        KmRate = kmRate;
+    }
+
+    public static Tariff Create(LocalDateTime date)
+    {
+        var (name, kmRate) = GetTariff(date);
+
+        return new Tariff(name, kmRate);
+    }
+
+    private static (string Name, float KmRate) Standard => ("Standard", 1.0f);
+    private static (string Name, float KmRate) NewYearsEve => ("Sylwester", 3.50f);
+    private static (string Name, float KmRate) Weekend => ("Weekend", 1.5f);
+    private static (string Name, float KmRate) WeekendPlus => ("Weekend+", 2.50f);
+
+    private static (string Name, float KmRate) GetTariff(LocalDateTime day)
+    {
+        // wprowadzenie nowych cennikow od 1.01.2019
+        if (day.Year <= 2018)
+        {
+            return Standard;
+        }
+
+        var year = day.Year;
+        var leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
+        if (((leap && day.DayOfYear == 366) || (!leap && day.DayOfYear == 365)) ||
+            (day.DayOfYear == 1 && day.Hour <= 6))
+        {
+            return NewYearsEve;
+        }
+        else
+        {
+            switch (day.DayOfWeek)
+            {
+                case IsoDayOfWeek.Monday:
+                case IsoDayOfWeek.Tuesday:
+                case IsoDayOfWeek.Wednesday:
+                case IsoDayOfWeek.Thursday:
+                    return Standard;
+                case IsoDayOfWeek.Friday:
+                    if (day.Hour < 17)
+                    {
+                        return Standard;
+                    }
+                    else
+                    {
+                        return WeekendPlus;
+                    }
+                case IsoDayOfWeek.Saturday:
+                    if (day.Hour < 6 || day.Hour >= 17)
+                    {
+                        return WeekendPlus;
+                    }
+                    else/* if (day.Hour < 17)*/
+                    {
+                        return Weekend;
+                    }
+                case IsoDayOfWeek.Sunday:
+                    if (day.Hour < 6)
+                    {
+                        return WeekendPlus;
+                    }
+                    else
+                    {
+                        return Weekend;
+                    }
+
+                default:
+                    return (default, default);
+            }
+        }
+
+    }
+}
